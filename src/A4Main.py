c4e6005a4fcf0484c249e1e2fa395d81aa3a749a
Adam Jaamour <adam@jamour.com>
2019-12-14 18:49:21 +0000

    Generate heatmap for confusion matrix + use debug mode to print info in terminal

--- src/A4Main.py
+++ src/A4Main.py
@@ -45,23 +45,23 @@ def create_multi_layer_perceptron(data):
         name=config.csv_file,
         input_data=data.input_data_encoded,
         target_data=data.target_data_encoded,
-        hidden_layers_size=(7,),  # Length = n_layers - 2. We only need 1 (n_units,)
-        solver='sgd',  # Stochastic Gradient Descent
+        hidden_layers_size=(7,),         # Length = n_layers - 2. We only need 1 (n_units,)
+        solver='sgd',                    # Stochastic Gradient Descent
         activation_function='logistic',  # Sigmoid activation function
-        learning_rate='constant',  # Don't increase learning rate during training
-        learning_rate_init=0.7,  # Learning rate
-        momentum=0.5,  # Momentum
-        optimisation_tolerance=0.0001,  # Stop condition: score not improving by this much for num_iterations_no_change
-        num_iterations_no_change=500,  # Stop condition: number of iterations with no change
-        max_iterations=10000,  # Stop condition: maximum number of iterations.
-        verbose=False,  # Print iterations at each step
+        learning_rate='constant',        # Don't increase learning rate during training
+        learning_rate_init=0.7,          # Learning rate
+        momentum=0.5,                    # Momentum
+        optimisation_tolerance=0.0001,   # Stop condition: score not improving by this much for num_iterations_no_change
+        num_iterations_no_change=500,    # Stop condition: number of iterations with no change
+        max_iterations=10000,            # Stop condition: maximum number of iterations.
+        verbose=config.debug,            # Print iterations at each step
     )
 
 
 def use_multi_layer_perceptron(mlp):
     mlp.split_data()
     mlp.train()
-    mlp.show_predictions()
+    mlp.make_predictions()
     mlp.show_results()
     mlp.save_trained_nn()
 
--- src/multi_layer_perceptron.py
+++ src/multi_layer_perceptron.py
@@ -1,10 +1,13 @@
 import joblib
 
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sn
 from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 
+import src.config as config
 from src.data import inverse_encoding, inverse_encoding_no_categories
 
 
@@ -41,26 +44,39 @@ class MultiLayerPerceptron:
     def train(self):
         self.mlp.fit(self.X_train, self.y_train)
 
-    def show_predictions(self):
+    def make_predictions(self):
         self.predictions = self.mlp.predict(self.X_test)
         probability_predictions = self.mlp.predict_proba(self.X_test)
-        for i, p in enumerate(inverse_encoding(self.predictions)):
-            print("{}\nPrediction = {}: {}\n".format(self.X_test.iloc[i].to_frame().T,
-                                                     self.categories[p],
-                                                     probability_predictions[i].tolist()))
+        if config.debug:
+            for i, p in enumerate(inverse_encoding(self.predictions)):
+                print("{}\nPrediction = {}: {}\n".format(self.X_test.iloc[i].to_frame().T,
+                                                         self.categories[p],
+                                                         probability_predictions[i].tolist()))
 
     def show_results(self):
+        # Plot error loss curve.
         plt.plot(self.mlp.loss_curve_)
         plt.xlabel("Epochs")
         plt.ylabel("Error loss")
         plt.show()
+
+        # Calculate confusion matrix
         ground_truth_values = inverse_encoding(self.y_test)
         estimated_target_values = inverse_encoding_no_categories(self.predictions, self.categories)
         cm = confusion_matrix(ground_truth_values, estimated_target_values)
-        cr = classification_report(ground_truth_values, estimated_target_values)
-        print(cm)
-        print()
-        print(cr)
+        # Convert numpy array to pandas DataFrame
+        cm = pd.DataFrame(data=cm, index=[i for i in self.categories], columns=[i for i in self.categories])
+
+        # Display confusion matrix as a heat map.
+        sn.heatmap(cm, annot=True, annot_kws={"size": 16}, cmap="YlGnBu")
+        plt.show()
+
+        if config.debug:
+            # Generate classification report and print confusion matrix and report to command line.
+            cr = classification_report(ground_truth_values, estimated_target_values)
+            print(cm)
+            print()
+            print(cr)
 
     def save_trained_nn(self):
         joblib.dump(self.mlp, "../neural_networks/{}.joblib".format(self.name))
