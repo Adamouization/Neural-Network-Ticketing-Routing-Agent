f6928977605e2356bbe78b373d8ff2146237b27f
Adam Jaamour <adam@jamour.com>
2019-12-15 01:37:21 +0000

    Measure runtime

--- src/grid_search_algorithm.py
+++ src/grid_search_algorithm.py
@@ -1,16 +1,18 @@
+import time
+
 import pandas as pd
 from pyspin.spin import Box1, make_spin
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.neural_network import MLPClassifier
 
 grid_search_params = {
-    'hidden_layer_sizes': [(5,), (7,), (9,), (20,)],
+    'hidden_layer_sizes': [(3,), (5,), (7,), (9,), (20,)],
     'solver': ["sgd", "adam"],
-    'activation': ["logistic", "relu"],
-    'learning_rate_init': [0.1, 0.5, 1.0],
-    'momentum': [0.1, 0.5, 1.0],
-    'tol': [0.01, 0.001, 0.0001],
-    'n_iter_no_change': [50, 500],
+    'activation': ["logistic", "relu", "tanh"],
+    'learning_rate_init': [0.1, 0.3, 0.7, 0.9],
+    'momentum': [0.1, 0.4, 0.7],
+    'tol': [0.1, 0.01, 0.001, 0.0001],
+    'n_iter_no_change': [100],
     'max_iter': [5000]
 }
 
@@ -33,7 +35,7 @@ class GridSearch:
         self.X_train, self.X_test, self.y_train, self.y_test = (None,) * 4
 
         self.split_data(input_data, target_data)
-        self.train()
+        self.run_grid_search()
         self.print_optimal_parameters()
         self.save_grid_search()
 
@@ -45,9 +47,16 @@ class GridSearch:
     @make_spin(
         Box1, "Running grid search algorithm on Multi-Layer Perceptron with {} unique combinations...".format(
             _number_unique_parameters_combination()))
-    def train(self):
-        print()
+    def run_grid_search(self):
+        # Start measuring runtime.
+        start_time = time.time()
+
+        # Run Grid Search using all possible combinations in grid_search_params.
         self.grid_search.fit(self.X_train, self.y_train)
+
+        # Record and print runtime.
+        runtime = round(time.time() - start_time, 2)
+        print("\n--- Grid Search Runtime: {} seconds ---".format(runtime))
 
     def print_optimal_parameters(self):
         print(self.grid_search.best_params_)
